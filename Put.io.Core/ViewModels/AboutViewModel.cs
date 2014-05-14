4113c52d24262ea110762ac9787176fb89686060
Simon Colmer <simon_colmer@hotmail.com>
2014-05-14 19:19:59 +0200

    Added about popup

---
+++ Put.io.Core/ViewModels/AboutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Put.io.Core.Common;
+
+namespace Put.io.Core.ViewModels
+{
+    public class AboutViewModel : ViewModelBase
+    {
+        public string Title { get { return "Put.io for Windows Phone"; } }
+        public string Description
+        {
+            get
+            {
+                return @"Thanks for using this overly simple application – I hope you enjoy it. This application is completely free and open source – feel free to contribute or branch it.
+
+If you like this application, please leave a review or even send me an email – I love to hear feedback.
+
+I dedicate this application to my Fiancé - Devika";
+            }
+        }
+        public string HomeUrl { get { return "http://x-volt.com"; } }
+        public string SourceUrl { get { return "https://github.com/Workshop2/Put.io.Wp"; } }
+        public string AuthorName { get { return "Simon Colmer"; } }
+        public string AuthorEmail { get { return "simon_colmer@hotmail.com"; } }
+        public string EmailSubject { get { return "Put.io for Windows Phone Feedback"; } }
+        public string EmailBody { get { return "This application is AWESOME....only kidding."; } }
+    }
+}
---
+++ Put.io.Wp/UserControls/AboutPopup.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Phone.Tasks;
+using Put.io.Core.ViewModels;
+using Put.io.Wp.UserControls.Popups;
+
+namespace Put.io.Wp.UserControls
+{
+    public partial class AboutPopup : IPopupClient
+    {
+        public event CloseHandler OnClose;
+        public event RedirectHandler OnRedirect;
+        public UserControl UserControl { get { return this; } }
+        private AboutViewModel Data { get { return DataContext as AboutViewModel; } }
+
+        public AboutPopup()
+        {
+            InitializeComponent();
+            DataContext = new AboutViewModel();
+        }
+
+        private void LeaveReview_OnClick(object sender, RoutedEventArgs e)
+        {
+            var task = new MarketplaceReviewTask();
+            task.Show();
+        }
+
+        private void VisitHomepage_OnClick(object sender, RoutedEventArgs e)
+        {
+            var task = new WebBrowserTask { Uri = new Uri(Data.HomeUrl, UriKind.Absolute) };
+            task.Show();
+        }
+
+        private void ViewSource_OnClick(object sender, RoutedEventArgs e)
+        {
+            var task = new WebBrowserTask { Uri = new Uri(Data.SourceUrl, UriKind.Absolute) };
+            task.Show();
+        }
+
+        private void EmailAuthor_OnClick(object sender, RoutedEventArgs e)
+        {
+            var task = new EmailComposeTask { To = Data.AuthorEmail, Subject = Data.EmailSubject, Body = Data.EmailBody };
+            task.Show();
+        }
+
+        public void Close()
+        {
+
+        }
+    }
+}
